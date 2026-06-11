0c1d2f6d487c48891f6175aa6ac10301e43f173d
William Rosser <will.e179@gmail.com>
2026-06-11 10:31:05 -0400

    Initial Commit.

---
+++ compile_motetorio.bat
@@ -0,0 +1,7 @@
+jar -cMf motetorio_core_0.1.0.zip motetorio_core_0.1.0
+xcopy /y motetorio_core_0.1.0.zip "C:\Users\wille\AppData\Roaming\Factorio\mods"
+jar -cMf motetorio_overhaul_0.1.0.zip motetorio_overhaul_0.1.0
+xcopy /y motetorio_overhaul_0.1.0.zip "C:\Users\wille\AppData\Roaming\Factorio\mods"
+jar -cMf arcane_musings_0.1.0.zip arcane_musings_0.1.0
+xcopy /y arcane_musings_0.1.0.zip "C:\Users\wille\AppData\Roaming\Factorio\mods"
+"C:\Program Files (x86)\Steam\steamapps\common\Factorio\bin\x64\factorio.exe"
